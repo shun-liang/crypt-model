f5bb2bae9f57d906b70482f64877db939c6b0032
Shun Liang <shun.liang.public@gmail.com>
2014-05-03 23:40:21 +0100

    Add scaled plotting function contents.

--- one_d/crypt_model.m
+++ one_d/crypt_model.m
@@ -127,19 +127,8 @@ average_clone_widths
 clone_probabilities = get_clone_probabilities(filtered_clone_widths, time_length, crypt_num);
 clone_probabilities
 
-figure;
 scaling_start = 10;
 scaling_end = 20;
-scaling_time_length = scaling_end - scaling_start + 1;
-cc=hsv(scaling_time_length);
-for t = scaling_start:scaling_end
-    clone_width_div_avg_clone_width = filtered_clone_widths(:, t + 1)./  average_clone_widths(...
-        :, t + 1);
-    clone_pro_times_avg_clone_width = clone_probabilities(:, t + 1).* average_clone_widths(...
-        :, t + 1);
-    plot(clone_width_div_avg_clone_width, clone_pro_times_avg_clone_width,...
-        '*', 'color', cc(t - scaling_start + 1,:));
-    hold on
-end
-m = legend(strsplit(num2str(scaling_start:scaling_end)));
-axis([0, 4, 0, 1.5]);
+
+plot_scaled_distributions(filtered_clone_widths, clone_probabilities,...
+    average_clone_widths, scaling_start, scaling_end);
--- plot_scaled_distributions.m
+++ plot_scaled_distributions.m
@@ -1,28 +1,18 @@
 % Plot the scalled distribution
-function plot_scaled_distribution(marked_cells, scaling_start, scaling_end)
-% marked_cells(c, t + 1) is the number of marked cells of crypt c at time t
+function plot_scaled_distributions(filtered_clone_widths, clone_probabilities,...
+    average_clone_widths, scaling_start, scaling_end)
+
 figure;
-%scaling_start = 1;
-%scaling_end = 2;
 scaling_time_length = scaling_end - scaling_start + 1;
 cc=hsv(scaling_time_length);
 for t = scaling_start:scaling_end
-    clone_widths_t = marked_cells(:, t + 1);
-    filter = clone_widths_t > 0 & clone_widths_t < n;
-    clone_widths_t_filtered = clone_widths_t(filter);
-%    clone_widths_count_t = sorted_marked_cells(scaling_start + t - 1, ...
-%        clone_widths_t_filtered + 1);
-%    clone_width_ratios_t_filtered = clone_widths_t_filtered./ n;
-%    avg_clone_width_t = mean(clone_widths_t_filtered);
-%    avg_clone_width_ratio_t = mean(clone_width_ratios_t_filtered);
-%    clone_pros_t = clone_widths_count_t./ size(clone_widths_t_filtered, 1) % crypt_num;
-%
-%    clone_widths_div_avg_width_t = clone_width_ratios_t_filtered./ avg_clone_width_ratio_t;
-%    clone_probs_t_div_avg_width_t = clone_pros_t.* avg_clone_width_ratio_t;
-    %plot(clone_widths_div_avg_width_t, clone_probs_t_div_avg_width_t, ...
-    %    '*', 'color', cc(t,:));
-    %plot(t, avg_clone_width_ratio_t, '*');
-    %plot(clone_pros_t, '*');
+    clone_width_div_avg_clone_width = filtered_clone_widths(:, t + 1)./  average_clone_widths(...
+        :, t + 1);
+    clone_pro_times_avg_clone_width = clone_probabilities(:, t + 1).* average_clone_widths(...
+        :, t + 1);
+    plot(clone_width_div_avg_clone_width, clone_pro_times_avg_clone_width,...
+        '*', 'color', cc(t - scaling_start + 1,:));
     hold on
 end
 m = legend(strsplit(num2str(scaling_start:scaling_end)));
+axis([0, 4, 0, 1.5]);
